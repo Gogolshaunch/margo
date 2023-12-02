db5874a8f3c1087c94712d31f1c0a733f5fada31
Gogolshaunch <130820886+Gogolshaunch@users.noreply.github.com>
2023-12-02 20:58:59 +0500

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,73 +1,46 @@
 from tkinter import *
-import tkinter.messagebox
-import pickle
-
 window = Tk()
-window.configure(bg="white")
-window.title("TO DO LIST")
+window.configure(bg="black")
+window.geometry("500x550")
+window.title("Калькулятор")
+window.resizable(False, False)
 
 
-def add_task():
-    task = entry_task.get()
-    if task != "":
-        task = f'●{task}'
-        listbox_tasks.insert(END, task)
-        entry_task.delete(0, END)
+def cal(operation):
+    global formula
+
+    if operation == 'C':
+        formula = ''
+    elif operation == 'del':
+        formula = formula[0: -1]
+    elif operation == '+/-':
+        formula = str((eval(formula)) * -1)
+    elif operation == '%':
+        formula = str((eval(formula)) * 100)
+    elif operation == 'X^2':
+        formula = str((eval(formula)) ** 2)
+    elif operation == '=':
+        formula = str(eval(formula))
     else:
-        tkinter.messagebox.showwarning(title="Warning!", message="Вы должны ввести задачу.")
+        if formula == '0':
+            formula = ''
+        formula += operation
+    label_text.configure(text=formula)
 
 
-def delete_task():
-    try:
-        task_index = listbox_tasks.curselection()[0]
-        listbox_tasks.delete(task_index)
-    except:
-        tkinter.messagebox.showwarning(title="Warning!", message="Вы должны выбрать задачу.")
+formula = '0'
+label_text = Label(text=formula, font=('Roboto', 40, 'bold'), bg='black', fg='white')
+label_text.place(x=25, y=60)
 
-
-def load_tasks():
-    try:
-        tasks = pickle.load(open("../tasks.dat", "rb"))
-        listbox_tasks.delete(0, tkinter.END)
-        for task in tasks:
-            listbox_tasks.insert(tkinter.END, task)
-    except:
-        tkinter.messagebox.showwarning(title="Warning!", message="Cannot find tasks.dat.")
-
-
-def save_tasks():
-    tasks = listbox_tasks.get(0, listbox_tasks.size())
-    pickle.dump(tasks, open("../tasks.dat", "wb"))
-
-
-frame_tasks = Frame(window)
-frame_tasks.pack()
-
-listbox_tasks = Listbox(frame_tasks, height=10, width=50)
-listbox_tasks.pack(side=tkinter.LEFT)
-
-scrollbar_tasks = Scrollbar(frame_tasks)
-scrollbar_tasks.pack(side=tkinter.RIGHT, fill=tkinter.Y)
-
-listbox_tasks.config(yscrollcommand=scrollbar_tasks.set)
-scrollbar_tasks.config(command=listbox_tasks.yview)
-
-lab = Label(window, text="Введите новые заметки: ", font=20, bg='white')
-lab.pack()
-
-entry_task = Entry(window, width=50)
-entry_task.pack()
-
-button_add_task = Button(window, text="Добавить задачу", width=48, command=add_task)
-button_add_task.pack()
-
-button_delete_task = Button(window, text="Удалить задачу", width=48, command=delete_task)
-button_delete_task.pack()
-
-button_load_tasks = tkinter.Button(window, text="Вывести сохраненные задачи", width=48, command=load_tasks)
-button_load_tasks.pack()
-
-button_save_tasks = Button(window, text="Сохранить задачу", width=48, command=save_tasks)
-button_save_tasks.pack()
+button = ['C', 'del', '*', '=', '1', '2', '3', '/', '4', '5', '6', '+', '7', '8', '9', '-', '+/-', '0', '%', 'X^2']
+x = 15
+y = 140
+for i in button:
+    get_lbl = lambda x = i: cal(x)
+    a = Button(text=i, bg='orange', font=('Roboto', 20), command=get_lbl).place(x=x, y=y, width=115, height=79)
+    x += 117
+    if x > 400:
+        x = 15
+        y += 81
 
 window.mainloop()
