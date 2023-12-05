c8ddd9350481ef4250ea98693e276fd378fc6c7b
Gogolshaunch <almira08_kz@mail.ru>
2023-12-05 20:15:13 +0500

    добавлены файлы ресурсов

--- inter.py
+++ inter.py
@@ -13,12 +13,12 @@ class Ui_MainWindow:
         self.micro = QtWidgets.QLabel(self.centralwidget)
         self.micro.setGeometry(QtCore.QRect(225, 40, 300, 300))
         self.micro.setText("")
-        self.micro.setPixmap(QtGui.QPixmap("../../Desktop/mir.png"))
+        self.micro.setPixmap(QtGui.QPixmap("image/mir.png"))
         self.micro.setObjectName("micro.py")
         self.voice = QtWidgets.QLabel(self.centralwidget)
         self.voice.setGeometry(QtCore.QRect(115, 275, 470, 240))
         self.voice.setText("")
-        self.voice.setPixmap(QtGui.QPixmap("../../Desktop/1.png"))
+        self.voice.setPixmap(QtGui.QPixmap("image/1.png"))
         self.voice.setObjectName("voice")
         MainWindow.setCentralWidget(self.centralwidget)
 
@@ -29,12 +29,12 @@ class Ui_MainWindow:
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         pushButton = QPushButton(self.centralwidget)
-        pushButton.setGeometry(QtCore.QRect(220, 470, 260, 40))
+        pushButton.setGeometry(QtCore.QRect(230, 470, 130, 40))
         pushButton.setText(_translate("MainWindow", "🎧"))
         pushButton.clicked.connect(assistant.run)
 
         makeButton = QPushButton(self.centralwidget)
-        makeButton.setGeometry(QtCore.QRect(220, 470, 260, 40))
+        makeButton.setGeometry(QtCore.QRect(330, 470, 130, 40))
         makeButton.setText(_translate("MainWindow", "+"))
         makeButton.clicked.connect(assistant.make_com)
 
