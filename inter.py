24b7ef76ef37f2bb88078b6de1ed4217dc6a3474
Gogolshaunch <130820886+Gogolshaunch@users.noreply.github.com>
2024-01-08 18:39:32 +0500

    Update inter.py

--- inter.py
+++ inter.py
@@ -1,10 +1,28 @@
+import json
+import os
+from threading import Thread
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QPushButton
-import configuration
 from assistant import Assistant
 
+with open('data_file.json', "r") as file:
+    configuration = json.load(file)
 
-class Ui_MainWindow:
+
+def increase():
+    assistant.run()
+
+
+def com():
+    os.system('python make_com')
+
+
+def com1():
+    t2 = Thread(target=com)
+    t2.start()
+
+
+class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(700, 550)
@@ -13,12 +31,12 @@ class Ui_MainWindow:
         self.micro = QtWidgets.QLabel(self.centralwidget)
         self.micro.setGeometry(QtCore.QRect(225, 40, 300, 300))
         self.micro.setText("")
-        self.micro.setPixmap(QtGui.QPixmap("image/mir.png"))
-        self.micro.setObjectName("micro.py")
+        self.micro.setPixmap(QtGui.QPixmap("mir.png"))
+        self.micro.setObjectName("micro")
         self.voice = QtWidgets.QLabel(self.centralwidget)
         self.voice.setGeometry(QtCore.QRect(115, 275, 470, 240))
         self.voice.setText("")
-        self.voice.setPixmap(QtGui.QPixmap("image/1.png"))
+        self.voice.setPixmap(QtGui.QPixmap("1.png"))
         self.voice.setObjectName("voice")
         MainWindow.setCentralWidget(self.centralwidget)
 
@@ -28,21 +46,19 @@ class Ui_MainWindow:
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        pushButton = QPushButton(self.centralwidget)
-        pushButton.setGeometry(QtCore.QRect(230, 470, 130, 40))
-        pushButton.setText(_translate("MainWindow", "🎧"))
-        pushButton.clicked.connect(assistant.run)
 
         makeButton = QPushButton(self.centralwidget)
-        makeButton.setGeometry(QtCore.QRect(330, 470, 130, 40))
+        makeButton.setGeometry(QtCore.QRect(220, 470, 260, 40))
         makeButton.setText(_translate("MainWindow", "+"))
-        makeButton.clicked.connect(assistant.make_com)
+        makeButton.clicked.connect(com1)
 
 
 if __name__ == "__main__":
     import sys
+    t1 = Thread(target=increase)
+    t1.start()
 
-    assistant = Assistant(configuration.commands)
+    assistant = Assistant(configuration)
     app = QtWidgets.QApplication(sys.argv)
 
     MainWindow = QtWidgets.QMainWindow()
